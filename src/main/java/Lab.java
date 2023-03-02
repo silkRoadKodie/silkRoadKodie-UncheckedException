922883fc94a3b8f928d5c7b5e685ad838a0c116c
Kodie Kuiper <antonio.pennyman@hyperisland.se>
2023-03-02 11:56:33 +0000

    return throwUncheckedException

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,6 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        throw new RuntimeException("This is an unchecked exception.");
     }
 }
